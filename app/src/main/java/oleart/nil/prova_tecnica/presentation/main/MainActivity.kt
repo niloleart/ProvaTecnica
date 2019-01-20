8d4a16341242297b57387ce08226053f6c5ab68d
noleart <noleart@opentrends.net>
2019-01-20 18:20:21 +0100

    conversions and rounding

--- app/src/main/java/oleart/nil/prova_tecnica/presentation/main/MainActivity.kt
+++ app/src/main/java/oleart/nil/prova_tecnica/presentation/main/MainActivity.kt
@@ -8,13 +8,17 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import oleart.nil.prova_tecnica.R
 import oleart.nil.prova_tecnica.domain.business.products.Product
+import oleart.nil.prova_tecnica.domain.business.products.ProductTransaction
 import oleart.nil.prova_tecnica.domain.business.rates.Rate
 import oleart.nil.prova_tecnica.general.GeneralActivity
 import oleart.nil.prova_tecnica.presentation.main.ui.TransactionsAdapter
 import java.lang.Exception
+import java.math.BigDecimal
+import java.math.RoundingMode
 import javax.inject.Inject
 
 class MainActivity : GeneralActivity(), AdapterView.OnItemSelectedListener {
+    val CURRENCY_EU = "EUR"
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -82,6 +86,7 @@ class MainActivity : GeneralActivity(), AdapterView.OnItemSelectedListener {
                     mSelectedProduct?.product?.let { showToast(it) }
                     mSelectedProduct?.transaction?.let { adapter.setList(it) }
                     adapter.notifyDataSetChanged()
+                    resultText.text = mSelectedProduct?.transaction?.let { computeResult(it) }
                 }
 
             }
@@ -101,5 +106,74 @@ class MainActivity : GeneralActivity(), AdapterView.OnItemSelectedListener {
         adapter = TransactionsAdapter(this)
         mainRV.adapter = adapter
         mSelectedProduct?.transaction?.let { adapter.setList(it) }
+    }
+
+    private fun computeResult(transactionList : MutableList<ProductTransaction>) : String {
+        var result : Double = 0.0
+        for (transaction in transactionList) {
+            result += when(transaction.currency) {
+                CURRENCY_EU -> transaction.amount
+                else-> convert(transaction)
+            }
+        }
+        return result.toString()
+    }
+
+
+    fun convert(transaction: ProductTransaction): Double {
+        val euroRates = getAllEuro()
+        val euroRatesIn = getAllInCurr(euroRates)
+        val ratesIn = getAllInCurr(mRates)
+
+        val conversionRate = mRates.find { it.inCurr == transaction.currency && it.outCurr == CURRENCY_EU }
+        if (conversionRate != null) return roundHalfEven( transaction.amount * conversionRate.rate)
+        else {
+            var interRates = mRates.filter { euroRatesIn.contains(it.outCurr) }
+            var firstRate = interRates.find { it.inCurr == transaction.currency }
+            if (firstRate != null) {
+                var euroRate = euroRates.find { it.inCurr == firstRate.outCurr }
+                if (euroRate != null) {
+                    return roundHalfEven(roundHalfEven(transaction.amount * euroRate.rate) * firstRate.rate)
+                }
+            } else {
+                var ratesWithTransactionCurrency = mRates.filter { ratesIn.contains(it.inCurr) }
+                var firstConversion = ratesWithTransactionCurrency.find {
+                    it.inCurr == transaction.currency && getAllInCurr(interRates.toMutableList()).contains(it.outCurr)
+                }
+                if (firstConversion != null) {
+                    var intermediateRate = interRates.find { it.inCurr == firstConversion.outCurr }
+                    if (intermediateRate != null) {
+                        var lastRate = euroRates.find { it.inCurr == intermediateRate.outCurr }
+                        if (lastRate != null) {
+                            return roundHalfEven(roundHalfEven(roundHalfEven(transaction.amount * firstConversion.rate) * intermediateRate.rate) * lastRate.rate)
+                        } else showToast("cagada")
+
+                    }
+                }
+            }
+
+        }
+        return 0.0
+    }
+
+
+    fun getAllEuro(): MutableList<Rate> {
+        var list = mutableListOf<Rate>()
+        for(rate in mRates) {
+            if (rate.outCurr == CURRENCY_EU) list.add(rate)
+        }
+        return list
+    }
+
+    private fun getAllInCurr(euroRates : MutableList<Rate>): MutableList<String> {
+        var listString = mutableListOf<String>()
+        for (rate in euroRates) {
+            listString.add(rate.inCurr)
+        }
+        return listString
+    }
+
+    private fun roundHalfEven(double : Double) : Double {
+        return BigDecimal(double).setScale(2, RoundingMode.HALF_EVEN).toDouble()
     }
 }
